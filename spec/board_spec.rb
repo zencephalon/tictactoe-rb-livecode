705af7d43bb4cf44ad0b5f1d2027cdd0f7768691
Matthew Bunday <zenceph@gmail.com>
2015-05-20 09:25:17 -0400

    Add place! method spec for board.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,4 +5,10 @@ describe Board do
     board = Board.new
     expect(board.to_s).to eq(" | | \n-----\n | | \n-----\n | | ")
   end
+
+  it "Should let us place pieces" do
+    board = Board.new
+    board.place!(0, 0, 'X')
+    expect(board.to_s).to eq("X| | \n-----\n | | \n-----\n | | ")
+  end
 end
