a53c5b6560d7bac9f56ca1067bf00ba55b3e07ec
Matthew Bunday <zenceph@gmail.com>
2015-05-20 09:26:45 -0400

    Add place! method to board.

--- board.rb
+++ board.rb
@@ -3,6 +3,10 @@ class Board
     @board = Array.new(3) { Array.new(3) { ' ' } }
   end
 
+  def place!(row, col, piece)
+    @board[row][col] = piece
+  end
+
   def to_s
     @board.map {|row| row.join('|') }.join("\n-----\n")
   end
